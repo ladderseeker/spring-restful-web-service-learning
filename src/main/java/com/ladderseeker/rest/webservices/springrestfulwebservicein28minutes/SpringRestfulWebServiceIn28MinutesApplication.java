08837e0ebd0e00cb0cfd62e0f6a8fd6c6ef5f580
XIAO <jinxiao.shawn@gmail.com>
2021-06-18 16:12:53 +0800

    Initialize the project and git.

---
+++ src/main/java/com/ladderseeker/rest/webservices/springrestfulwebservicein28minutes/SpringRestfulWebServiceIn28MinutesApplication.java
@@ -0,0 +1,13 @@
+package com.ladderseeker.rest.webservices.springrestfulwebservicein28minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulWebServiceIn28MinutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestfulWebServiceIn28MinutesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ladderseeker/rest/webservices/springrestfulwebservicein28minutes/SpringRestfulWebServiceIn28MinutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladderseeker.rest.webservices.springrestfulwebservicein28minutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulWebServiceIn28MinutesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
